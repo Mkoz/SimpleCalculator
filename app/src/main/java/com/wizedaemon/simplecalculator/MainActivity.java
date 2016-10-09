79915f8f8e773b782899f747dab5d85fa5038efe
Mikhail Kozyrev <mefistofel_89@list.ru>
2016-10-09 21:12:17 +0300

    Theme v0.2

--- app/src/main/java/com/wizedaemon/simplecalculator/MainActivity.java
+++ app/src/main/java/com/wizedaemon/simplecalculator/MainActivity.java
@@ -1,8 +1,11 @@
 package com.wizedaemon.simplecalculator;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,18 +25,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnSub;
     Button btnMult;
     Button btnDiv;
+    Button btnStyle1;
+    Button btnStyle2;
+    Button btnStyle3;
 
     TextView tvResult;
 
     String oper = "";
+    Activity this_act;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //ThemeChanger.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_main);
 
         // находим элементы
+        this_act = this.getParent();
         etNum1 = (EditText) findViewById(R.id.etNum1);
         etNum2 = (EditText) findViewById(R.id.etNum2);
 
@@ -44,11 +53,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         tvResult = (TextView) findViewById(R.id.tvResult);
 
+        btnStyle1 = (Button) findViewById(R.id.style_button1);
+        btnStyle2 = (Button) findViewById(R.id.style_button2);
+        btnStyle3 = (Button) findViewById(R.id.style_button3);
+
         // прописываем обработчик
         btnAdd.setOnClickListener(this);
         btnSub.setOnClickListener(this);
         btnMult.setOnClickListener(this);
         btnDiv.setOnClickListener(this);
+        btnStyle1.setOnClickListener(Style);
+        btnStyle2.setOnClickListener(Style);
+        btnStyle3.setOnClickListener(Style);
 
     }
 
@@ -123,4 +139,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public View.OnClickListener Style = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId())
+            {
+
+                case R.id.style_button1:
+                    ThemeChanger.changeToTheme(this_act,ThemeChanger.STYLE1);
+                    break;
+                case R.id.style_button2:
+                    ThemeChanger.changeToTheme(this_act,ThemeChanger.STYLE2);
+                    break;
+                case R.id.style_button3:
+                    ThemeChanger.changeToTheme(this_act,ThemeChanger.STYLE3);
+                    break;
+            }
+        }
+
+    };
 }
